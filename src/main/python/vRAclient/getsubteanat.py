f7a31498a21aab0e88a673f5d0ede989705c0112
hkalidex <99737347+hkalidex@users.noreply.github.com>
2022-12-28 20:40:04 +0530

    Update getsubteanat.py

--- src/main/python/vRAclient/getsubteanat.py
+++ src/main/python/vRAclient/getsubteanat.py
@@ -1,12 +1,28 @@
+import os
+import json
+from time import sleep
+from datetime import datetime
+from datetime import timedelta
+import requests
+from retrying import retry
+from RESTclient import RESTclient
 
+import logging
+logger = logging.getLogger(__name__)
+
+logging.getLogger('urllib3.connectionpool').setLevel(logging.CRITICAL)
+
+
+VRA_HOST = 'vradev.fms07vraapp101.fm.intel.com'
+VRA_TENANT = 'VRADEV'
+
+def get_subtenants(access_token):
 url = 'https://{}/iaas/api/projects'.format(hostname)
-data1 = {
-        " Token" : raiserer_token }
-data1 =  json.dumps ( data1 )
-print ( data1 )
+
+
 headers = {
        'accept': "application/json",
-       'authorization': raiserer_token
+       'authorization': access_token
        }
-api_output = requests.request("GET",url, headers=headers, verify='/etc/ssl/certs/cabundle.pem').json()['content']
+api_output = requests.request("GET",url, headers=headers,  verify=RESTclient.cabundle').json()['content']
 return api_output
