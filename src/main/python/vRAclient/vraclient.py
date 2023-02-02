014ff46462eb7179e2cef2983ab5e81a0e16a712
hkalidex <99737347+hkalidex@users.noreply.github.com>
2023-02-02 22:25:11 +0530

    Update vraclient.py

--- src/main/python/vRAclient/vraclient.py
+++ src/main/python/vRAclient/vraclient.py
@@ -276,7 +276,7 @@ class vRAclient(RESTclient):
         return result
     
     def get_resources_deploymentsapi_new(self, access_token, hostname):
-        url = "https://{}/deployment/api/deployments?resourceTypes=Cloud.vSphere.Machine".format(hostname)
+        url = "https://{}/deployment/api/deployments?resourceTypes=Cloud.vSphere.Machine&size=200".format(hostname)
         headers = {
                 'accept': 'application/json',
                 'authorization': access_token
